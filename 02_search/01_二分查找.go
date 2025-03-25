cc37036f740c4befaa9d392b2bab58a27c5f325d
张佳星 <jiaxing0901@163.com>
2025-03-25 15:49:37 +0800

    feat: 优化二分查找实现

--- 02_search/01_二分查找.go
+++ 02_search/01_二分查找.go
@@ -9,8 +9,7 @@ func BinarySearch(nums []int, target int) int {
 		mid := left + (right-left)/2
 		if nums[mid] == target {
 			return mid
-		}
-		if nums[mid] < target {
+		} else if nums[mid] < target {
 			left = mid + 1
 		} else {
 			right = mid - 1
